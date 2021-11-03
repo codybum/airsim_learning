6feef5923dc56a892e40b80120d942f0a23740a0
codybum <cbumgardner@gmail.com>
2021-11-03 18:29:25 -0400

    kickit

--- airgym/envs/drone_env.py
+++ airgym/envs/drone_env.py
@@ -82,7 +82,7 @@ class AirSimDroneEnv(AirSimEnv):
             quad_vel.x_val + quad_offset[0],
             quad_vel.y_val + quad_offset[1],
             quad_vel.z_val + quad_offset[2],
-            5,vehicle_name=self.ident,
+            5, vehicle_name=self.ident,
         ).join()
 
     def _compute_reward(self):
